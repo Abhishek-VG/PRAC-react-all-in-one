4e7b1c00ec153b4cb297f418244092fb383ea702
Abhishek B V <VAbhishek@ESTL-AbhishekG.estuate.com>
2018-05-22 22:55:26 +0530

    added

---
+++ src/lifecycles/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Lifecycle from './lifecycle'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false,
+      score: 0,
+      localNum: 0,//ti trigger update which doesnt affects props of lifecycle comp but still renders
+    }
+    this.initialState = this.state
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron text-center">
+          <button className="btn btn-primary" onClick={() => this.setState({ display: !this.state.display })}>
+            Display Lifecycle
+         </button>
+          <button className="btn btn-success" onClick={() => this.setState({ score: ++this.state.score })}>
+            Score ++ ({this.state.score})
+          </button>
+          <button className="btn btn-warning" onClick={() => this.setState({ score: this.state.score })}>
+            Send same props
+          </button>
+          <button className="btn btn-danger" onClick={() => this.setState(this.initialState)}>
+            Reset
+          </button>
+          <button className="btn" onClick={() => this.setState({localNum: ++this.state.localNum})}>
+            Local count ++ ({this.state.localNum})
+          </button>
+        </div>
+        {this.state.display && <Lifecycle score={this.state.score} />}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/lifecycles/lifecycle.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Lifecycle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { redColor: true }
+    console.log("constructor props ---> ", props)
+  }
+  componentWillMount() {
+    console.log('componentWillMount ---> no arguments')
+  }
+  componentDidMount() {
+    console.log('componentDidMount ---> no arguments')
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps ---> ', nextProps)
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('shouldComponentUpdate ---> nextProps = ', nextProps,' nextState = ' , nextState)
+    // if((nextState === this.state) && (nextProps === this.props)) {
+    //   return false;
+    // }
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log('componentWillUpdate ---> nextProps = ', nextProps,' nextState = ' , nextState)
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate ---> prevProps = ', prevProps, ' prevState = ', prevState)
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount ---> no arguments')
+  }
+  render() {
+    console.log('*****Render*****')
+    const color = this.state.redColor ? 'danger' : 'info';
+    return (
+      <div className="well well-lg  text-center">
+        <button className={`btn btn-${color}`} onClick={() => this.setState({ redColor: !this.state.redColor })}>
+          Update lifecycle component
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Lifecycle;
