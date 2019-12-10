7bb6ced2dfba0097ad57120f8294d2410a4c3e44
Abhishek Guruswamy <guruab@kredifi.net>
2019-12-10 23:28:45 +0530

    Lazyload added in testng

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './lifecycles';
+import Lifecycle from './lifecycles';
+import Lazyload from './lazyload'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Lazyload />, document.getElementById('root'));
--- src/lifecycles/index.js
+++ src/lifecycles/index.js
@@ -19,7 +19,7 @@ class App extends Component {
           <button className="btn btn-primary" onClick={() => this.setState({ display: !this.state.display })}>
             Display Lifecycle
          </button>
-          <button className="btn btn-success" onClick={() => this.setState({ score: ++this.state.score })}>
+          <button className="btn btn-success" onClick={() => this.setState({ score: this.state.score+1 })}>
             Score ++ ({this.state.score})
           </button>
           <button className="btn btn-warning" onClick={() => this.setState({ score: this.state.score })}>
@@ -28,7 +28,7 @@ class App extends Component {
           <button className="btn btn-danger" onClick={() => this.setState(this.initialState)}>
             Reset
           </button>
-          <button className="btn" onClick={() => this.setState({localNum: ++this.state.localNum})}>
+          <button className="btn" onClick={() => this.setState({localNum: this.state.localNum+1})}>
             Local count ++ ({this.state.localNum})
           </button>
         </div>
