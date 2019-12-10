c4b9ce86109420cefa0e091cbf9b5e66fea55270
Abhishek Guruswamy <guruab@kredifi.net>
2019-12-10 23:33:01 +0530

    Lazyload added in testng

---
+++ src/lazyload/lazyloadHelper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LazyloaderHelper extends Component {
+  render() {
+    return (
+      <div>
+          Working.....
+      </div>
+    )
+  }
+}
+
+export default LazyloaderHelper;
